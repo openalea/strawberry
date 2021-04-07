50e03ca37dbbde7dcb462eb5e9da64fbcb3ad3b0
pomme-abricot <gaetan.heidsieck@gmail.com>
2021-04-07 18:14:36 +0200

    fix test extract all genotype

--- test/test_extraction.py
+++ test/test_extraction.py
@@ -2,6 +2,8 @@ from openalea.deploy.shared_data import shared_data
 import openalea.strawberry
 from openalea.mtg.io import read_mtg_file
 from openalea.mtg.algo import union
+from openalea.mtg import MTG
+
 
 from openalea.strawberry.analysis import extract_at_module_scale, extract_at_node_scale, extract_at_plant_scale
 
@@ -12,51 +14,60 @@ def test_extract_at_module_scale():
     files = shared_data(openalea.strawberry).glob('*.mtg')
 
     mtg_path = dict((name(f), f) for f in files)
-    mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
-    mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
-    mtg = union(mtg1, mtg2)
-    genotype = list(mtg_path)[0]
+    mtg = MTG()
+    for k, genotype in enumerate(mtg_path):
+        if (genotype=="Sicile") or (genotype=="Nils") or (genotype=='origine_stolon_gariguetteDV_6novembre2018') \
+        or (genotype=='friendlyfruit_varieties'):
+            pass
+        else:
+            mtg = union(mtg, read_mtg_file(mtg_path[genotype]))
 
     genotypes = mtg.property('Genotype')
-    clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
 
-    df = extract_at_module_scale(mtg, clery_vids)
-
-    assert not df.empty
-    assert set(df['Genotype']) == {genotype}
+    for geno in set(genotypes.values()):
+        vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == geno]
+        df = extract_at_module_scale(mtg, vids)
+        assert not df.empty
+        assert set(df['Genotype']) == {geno}
 
 
 def test_extract_at_node_scale():
     files = shared_data(openalea.strawberry).glob('*.mtg')
 
     mtg_path = dict((name(f), f) for f in files)
-    mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
-    mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
-    mtg = union(mtg1, mtg2)
-    genotype = list(mtg_path)[0]
+    mtg = MTG()
+    for k, genotype in enumerate(mtg_path):
+        if (genotype=="Sicile") or (genotype=="Nils") or (genotype=='origine_stolon_gariguetteDV_6novembre2018') \
+        or (genotype=='friendlyfruit_varieties'):
+            pass
+        else:
+            mtg = union(mtg, read_mtg_file(mtg_path[genotype]))
 
     genotypes = mtg.property('Genotype')
-    clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
 
-    df = extract_at_node_scale(mtg, clery_vids)
-
-    assert not df.empty
-    assert set(df['Genotype']) == {genotype}
+    for geno in set(genotypes.values()):
+        vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == geno]
+        df = extract_at_node_scale(mtg, vids)
+        assert not df.empty
+        assert set(df['Genotype']) == {geno}
 
 
 def test_extract_at_plant_scale():
     files = shared_data(openalea.strawberry).glob('*.mtg')
 
     mtg_path = dict((name(f), f) for f in files)
-    mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
-    mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
-    mtg = union(mtg1, mtg2)
-    genotype = list(mtg_path)[0]
+    mtg = MTG()
+    for k, genotype in enumerate(mtg_path):
+        if (genotype=="Sicile") or (genotype=="Nils") or (genotype=='origine_stolon_gariguetteDV_6novembre2018') \
+        or (genotype=='friendlyfruit_varieties'):
+            pass
+        else:
+            mtg = union(mtg, read_mtg_file(mtg_path[genotype]))
 
     genotypes = mtg.property('Genotype')
-    clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
 
-    df = extract_at_plant_scale(mtg, clery_vids)
-
-    assert not df.empty
-    assert set(df['Genotype']) == {genotype}
+    for geno in set(genotypes.values()):
+        vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == geno]
+        df = extract_at_plant_scale(mtg, vids)
+        assert not df.empty
+        assert set(df['Genotype']) == {geno}
