b08131b16af4fb0ca8ddbb183a5323c46a30fa75
pradal <christophe.pradal@inria.fr>
2021-04-07 11:52:49 +0200

    Port code to multi_OS: replace namebase by a name function

--- test/extraction.py
+++ test/extraction.py
@@ -5,13 +5,17 @@ from openalea.mtg.algo import union
 
 from openalea.strawberry.variables import extract_at_module_scale, extract_at_node_scale, extract_at_plant_scale
 
-def test_extract_at_module_scale(self):
+def name(f):
+    return f.basename().splitext()[0]
+
+def test_extract_at_module_scale():
     files = shared_data(openalea.strawberry).glob('*.mtg')
-    mtg_path = dict((f.namebase, f) for f in files)
+
+    mtg_path = dict((name(f), f) for f in files)
     mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
     mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
     mtg = union(mtg1, mtg2)
-    genotype = mtg_path[list(mtg_path)[0]].namebase
+    genotype = list(mtg_path)[0]
 
     genotypes = mtg.property('Genotype')
     clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
