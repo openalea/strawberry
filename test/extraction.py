1348673e4088d28085f07bcc991f1ea0360a9773
pomme-abricot <gaetan.heidsieck@gmail.com>
2021-04-07 11:59:20 +0200

    add plant & node scale

--- test/extraction.py
+++ test/extraction.py
@@ -26,3 +26,37 @@ def test_extract_at_module_scale():
     assert set(df['Genotype']) == {genotype}
 
 
+def test_extract_at_node_scale()):
+    files = shared_data(openalea.strawberry).glob('*.mtg')
+
+    mtg_path = dict((name(f), f) for f in files)
+    mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
+    mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
+    mtg = union(mtg1, mtg2)
+    genotype = list(mtg_path)[0]
+
+    genotypes = mtg.property('Genotype')
+    clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
+
+    df = extract_at_node_scale(mtg, clery_vids)
+
+    assert not df.empty
+    assert set(df['Genotype']) == {genotype}
+
+
+def test_extract_at_plant_scale():
+    files = shared_data(openalea.strawberry).glob('*.mtg')
+
+    mtg_path = dict((name(f), f) for f in files)
+    mtg1 = read_mtg_file(mtg_path[list(mtg_path)[0]])
+    mtg2 = read_mtg_file(mtg_path[list(mtg_path)[1]])
+    mtg = union(mtg1, mtg2)
+    genotype = list(mtg_path)[0]
+
+    genotypes = mtg.property('Genotype')
+    clery_vids=[vid for vid in mtg.vertices(scale=1) if genotypes.get(vid) == genotype]
+
+    df = extract_at_plant_scale(mtg, clery_vids)
+
+    assert not df.empty
+    assert set(df['Genotype']) == {genotype}
