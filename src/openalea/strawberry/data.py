c69a59ba8aaeb9f6057df0c4965d79bfaec5ac9e
pradal <christophe.pradal@cirad.fr>
2024-02-12 17:28:09 +0100

    Add single file to manage shared_data

---
+++ src/openalea/strawberry/data.py
@@ -0,0 +1,14 @@
+""" Module providing shared data access
+"""
+
+from openalea.deploy.shared_data import shared_data
+
+
+data_directory = shared_data(openalea.strawberry.__path__)
+count = 0
+d = Path(openalea.strawberry.__path__[0])
+while data_directory is None and count <=3:
+    d = d/'..'
+    data_directory = shared_data(d)
+    count+=1
+
